93bd0525f264194c76e461065ebce5141ba90294
dnyana <dnyaneshgawade903@gmail.com>
2021-09-05 18:11:08 +0530

    [Dnyanesh] Add :create new table

---
+++ AddressBookServiceADO/AddressBookOperations.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace AddressBookServiceADO
+{
+    public class AddressBookOperations
+    {
+        public static string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=AddressBookServiceADO";
+        SqlConnection sqlConnection = new SqlConnection(connectionString);
+    }
+}
--- AddressBookServiceDatabase.sql
+++ AddressBookServiceDatabase.sql
@@ -2,3 +2,15 @@ create database AddressBookServiceADO
 
 
 use AddressBookServiceADO
+
+create table AddressBook
+(
+	FirstName varchar(20) not null,
+	LastName varchar(20) not null,
+	Address varchar(20) not null,
+	City varchar(20) not null,
+	State varchar(20) not null,
+	Zip float not null,
+	PhoneNumber float not null,
+	Email varchar(50) not null
+)
