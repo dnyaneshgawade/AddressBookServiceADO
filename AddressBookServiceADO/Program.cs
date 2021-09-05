90d8a1ed2a3060aae5978516a65be8995bb80c1c
dnyana <dnyaneshgawade903@gmail.com>
2021-09-05 17:36:58 +0530

    [Dnyanesh] Add :create and connect to the database

---
+++ AddressBookServiceADO/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookServiceADO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome To Address Book Service!");
+        }
+    }
+}
---
+++ AddressBookServiceDatabase.sql
@@ -0,0 +1,4 @@
+create database AddressBookServiceADO
+
+
+use AddressBookServiceADO
